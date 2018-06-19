8c97bae536ad6a5b71d90b81948dfe19ec2ac4a6
Devo <the.devin.carter@outlook.com>
2018-06-18 21:53:52 -0500

    generate shopping list component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    ShoppingListComponent
   ],
   imports: [
     BrowserModule
